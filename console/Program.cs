04b5c24009fde227494eb8402ab3b0d22fb625f0
Shaun Luttin <shaunluttin@gmail.com>
2018-10-11 21:47:47 -0700

    Initial commit

---
+++ console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            const string obj = @"
+{
+    'Cart': {
+        'Product': [
+            {
+                'Sku': '123',
+                'Size': 'S',
+                'quantity': '1'
+            },
+            {
+                'Sku': '456',
+                'Size': 'M',
+                'quantity': '2'
+            },
+            {
+                'Sku': '789',
+                'Size': 'L',
+                'quantity': '3'
+            }
+        ],
+        'ShipToZip': '54452',
+        'ShipMethod': 'Ground'
+    }
+}";
+
+            var content = new StringContent(obj.ToString(), Encoding.UTF8, "application/json");
+            var result = client.PostAsync("https://localhost:5001/api/values", content).GetAwaiter().GetResult();
+
+            Console.WriteLine(result);
+            Console.WriteLine(result.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+        }
+    }
+}
---
+++ webapi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_web_api_array_in_post.Controllers
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+        public string Size { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Cart
+    {
+        public List<Product> Product { get; set; }
+        public string ShipToZip { get; set; }
+        public string ShipMethod { get; set; }
+    }
+
+    public class CartBody
+    {
+        public Cart Cart { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public ActionResult<CartBody> Post(CartBody cartBody)
+        {
+            return cartBody;
+        }
+    }
+}
